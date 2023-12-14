ff854ae956c1ae335e9d1678787bad81ad8d0823
jiminh03 <101847439+jiminh03@users.noreply.github.com>
2023-12-15 01:50:03 +0900

    Update elbow_timestamp.py

--- clustering/elbow_timestamp.py
+++ clustering/elbow_timestamp.py
@@ -11,16 +11,16 @@ excel_file_path = 'C:/Users/jamie/Downloads/tags.xlsx'
 # 엑셀 파일 읽기
 movies = pd.read_excel(excel_file_path)
 
-# 장르 정보 전처리
+# timestamp 정보 전처리
 mlb = MultiLabelBinarizer()
-genres_matrix = mlb.fit_transform(movies['timestamp'].apply(lambda x: str(x).split('|')))
-genres_df = pd.DataFrame(genres_matrix, columns=mlb.classes_)
+timestamps_matrix = mlb.fit_transform(movies['timestamp'].apply(lambda x: str(x).split('|')))
+timestamps_df = pd.DataFrame(timestamps_matrix, columns=mlb.classes_)
 
 # KMeans 모델 훈련 및 Elbow Method 적용
 sse = []
 for k in range(1, 11):
     kmeans = KMeans(n_clusters=k, random_state=42)
-    kmeans.fit(genres_df)
+    kmeans.fit(timestamps_df)
     sse.append(kmeans.inertia_)
 
 # Elbow Method 그래프 그리기
@@ -33,19 +33,19 @@ plt.show()
 # Optimal k를 선택하여 KMeans 모델 재훈련
 optimal_k = 4  # Elbow Method 그래프를 통해 최적의 k를 선택
 kmeans_optimal = KMeans(n_clusters=optimal_k, random_state=42)
-genres_df['cluster'] = kmeans_optimal.fit_predict(genres_df)
+timestamps_df['cluster'] = kmeans_optimal.fit_predict(timestamps_df)
 
 # 클러스터 결과 확인
-cluster_counts = genres_df['cluster'].value_counts()
+cluster_counts = timestamps_df['cluster'].value_counts()
 print(cluster_counts)
 
 # 차원 축소 및 시각화 (예시로 2차원으로 축소)
 pca = PCA(n_components=2)
-genres_pca = pca.fit_transform(genres_df.drop('cluster', axis=1))
-genres_df[['pca1', 'pca2']] = genres_pca
+timestamps_pca = pca.fit_transform(timestamps_df.drop('cluster', axis=1))
+timestamps_df[['pca1', 'pca2']] = timestamps_pca
 
 # 클러스터링 결과 시각화
-plt.scatter(genres_df['pca1'], genres_df['pca2'], c=genres_df['cluster'], cmap='viridis')
+plt.scatter(timestamps_df['pca1'], timestamps_df['pca2'], c=timestamps_df['cluster'], cmap='viridis')
 plt.title('Clustering Results')
 plt.xlabel('PCA1')
 plt.ylabel('PCA2')
